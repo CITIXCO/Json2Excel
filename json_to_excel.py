f0df81cb630e2c5c5ff149365f974599f14fa7fb
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-07-21 14:11:13 -0500

    dict labels

--- json_to_excel.py
+++ json_to_excel.py
@@ -1,3 +1,25 @@
+import json
 import pandas as pd
-df_json = pd.read_json('map.geojson')
-df_json.to_excel('map.xlsx')
+
+
+with open('map.json') as json_file:
+    data = json.load(json_file)
+
+
+result = []
+for item in data["features"]:
+    if item.get('geometry').get('type') == "Polygon" and item.get('properties').get('Name'):
+        my_dict = {}
+        my_dict['price'] = item.get('properties').get('Name')
+        my_dict['polygon'] = []
+        my_dict['city_id'] = 118
+        my_dict['vehicle_type_id'] = 3
+        my_dict['round_trip_percent'] = 20
+        my_dict['comapany_id'] = 9
+        if len(item.get('geometry').get('coordinates')):
+            my_dict['polygon'] = item.get('geometry').get('coordinates')[0]
+
+        result.append(my_dict)
+
+df = pd.DataFrame(result)
+df.to_excel('map.xlsx')
