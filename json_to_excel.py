4104111e6a0d8cab579c66f2d6c6f90ee54dfd3f
Sebastian Manrique <yhoan.manrique@correo.uis.edu.co>
2021-07-21 14:33:29 -0500

    only int values

--- json_to_excel.py
+++ json_to_excel.py
@@ -8,14 +8,19 @@ with open('map.json') as json_file:
 
 result = []
 for item in data["features"]:
-    if item.get('geometry').get('type') == "Polygon" and item.get('properties').get('Name'):
+    try:
+        temp_price = int(item.get('properties').get('Name'))
+    except ValueError:
+        temp_price = 0
+
+    if item.get('geometry').get('type') == "Polygon" and item.get('properties').get('Name') and temp_price:
         my_dict = {}
         my_dict['polygon'] = []
-        my_dict['price'] = item.get('properties').get('Name')
+        my_dict['price'] = temp_price
         my_dict['city_id'] = 118
         my_dict['vehicle_type_id'] = 3
         my_dict['round_trip_percent'] = 20
-        my_dict['comapany_id'] = 9
+        my_dict['company_id'] = 9
         if len(item.get('geometry').get('coordinates')):
             my_dict['polygon'] = item.get('geometry').get('coordinates')[0]
 
